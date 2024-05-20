077fcd41d558a2a4f714315bf5888a00602f9cf8
unknown <moaedadres22@gmail.com>
2024-05-20 21:21:39 +0300

    env file

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,12 +1,13 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  username: 'postgres',
-  database: 'postgres',
-  password: '12345678',
-  host: 'localhost',
-  port: 5444,
+  username: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
 };
